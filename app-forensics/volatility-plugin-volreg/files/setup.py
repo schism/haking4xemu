e2eb18018eb3675cb5083509cee5af51ea9bf1db
dacook <schism@subverted.org>
2009-08-08 10:30:12 -0600

    volreg-0.6 setup.py tweak
    Minimize the contents of setup.py, some entries were unnecessary.

--- app-forensics/volatility-plugin-volreg/files/setup.py
+++ app-forensics/volatility-plugin-volreg/files/setup.py
@@ -2,5 +2,5 @@
 from distutils.core import setup
 setup(name='volreg',
     version='0.6',
-    packages=["forensics", "forensics.win32","memory_plugins","memory_plugins.registry","memory_objects","memory_objects.Windows"],
+    packages=["forensics.win32","memory_plugins.registry","memory_objects.Windows"],
 )
